0bf842bbda2a052c30b558b2dbfde36dc85e820d
MQady <m.qad@neopplanet.com>
2022-08-07 12:56:10 +0200

    WOP

--- routes/admin.php
+++ routes/admin.php
@@ -7,5 +7,10 @@ use Neop\Blog\Http\Controllers\Admin\BlogTagsController;
 Route::middleware(['web', 'auth:admin'])->name('admin.')->prefix('admin')->group(function () {
 
    Route::resource('posts', BlogController::class)->except('show');
-   Route::resource('posts/tags', BlogTagsController::class)->name('index', 'posts.')->except('show');
+
+   Route::name('posts.')->prefix('posts')->group(
+      function () {
+         Route::resource('tags', BlogTagsController::class)->except('show');
+      }
+   );
 });
