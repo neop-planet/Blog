553508e7411313e34ad1d528992f04e224e62744
MQady <m.qad@neopplanet.com>
2022-08-08 11:44:13 +0200

    Update create_blog_tags_table.php

--- database/migrations/create_blog_tags_table.php
+++ database/migrations/create_blog_tags_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('post_tags', function (Blueprint $table) {
+        Schema::create('blog_tags', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->timestamps();
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('post_tags');
+        Schema::dropIfExists('blog_tags');
     }
 };
