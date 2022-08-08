efe98bb04552f1e73a5c17f5e0c6df4df6e62ea4
MQady <m.qad@neopplanet.com>
2022-08-08 10:16:11 +0200

    Update BlogServiceProvider.php

--- src/Providers/BlogServiceProvider.php
+++ src/Providers/BlogServiceProvider.php
@@ -13,13 +13,14 @@ class BlogServiceProvider extends ServiceProvider
 
       $this->loadViewsFrom(__DIR__ . '/../../resources/views/', 'neop-blog');
 
+      $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
 
-      $this->publishes([
-         // __DIR__ . '/../../resources/views/' => resource_path('views/neop/'),
-         __DIR__ . '/../../database/migrations/create_posts_table.php' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_posts_table.php'),
-         __DIR__ . '/../../database/migrations/create_blog_tags_table.php' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_blog_tags_table.php'),
-         __DIR__ . '/../../database/migrations/create_blog_tag_post_table.php' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_blog_tag_post_table.php'),
-      ], 'laravel-assets');
+      // $this->publishes([
+      //    // __DIR__ . '/../../resources/views/' => resource_path('views/neop/'),
+      //    __DIR__ . '/../../database/migrations/create_posts_table.php' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_posts_table.php'),
+      //    __DIR__ . '/../../database/migrations/create_blog_tags_table.php' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_blog_tags_table.php'),
+      //    __DIR__ . '/../../database/migrations/create_blog_tag_post_table.php' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_blog_tag_post_table.php'),
+      // ], 'laravel-assets');
 
 
       // $this->publishes([
