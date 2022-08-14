0b727fdb0caf2d52603d04a90207e1ea9a6c3bab
MQady <m.qad@neopplanet.com>
2022-08-14 11:26:57 +0200

    Change migrations to json

--- database/migrations/create_posts_table.php
+++ database/migrations/create_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('title')->unique();
+            $table->json('title');
             $table->string('slug', 300)->unique();
             $table->string('image')->nullable();
             $table->json('content');
